54f8c3960103f0634db03b9183c17173eeb802ef
Vinicius <vinicius_marcondes_esteter@hotmail.com>
2022-02-13 20:12:08 -0300

    Alterado para testes com entregas

--- sql_script.sql
+++ sql_script.sql
@@ -1,14 +1,14 @@
 INSERT INTO `credible-skill-341215.Alunos.Tabela`(ID,Nome,Telefone,Matricula,Status)
-values (2,"Maria",34267699,8888,"Inativo")
+values (1,"Maria",34267699,8888,"Inativo")
 
 INSERT INTO `credible-skill-341215.Alunos.Tabela`(ID,Nome,Telefone,Matricula,Status)
-values (3,"Joaquim",34267677,7777,"Inativo")
+values (2,"Joaquim",34267677,7777,"Inativo")
 
 INSERT INTO `credible-skill-341215.Alunos.Tabela`(ID,Nome,Telefone,Matricula,Status)
-values (4,"Luis",34267666,6666,"Inativo")
+values (3,"Luis",34267666,6666,"Inativo")
 
 INSERT INTO `credible-skill-341215.Alunos.Tabela`(ID,Nome,Telefone,Matricula,Status)
-values (5,"Julia",34267655,5555,"Ativado")
+values (4,"Julia",34267655,5555,"Ativado")
 
 INSERT INTO `credible-skill-341215.Alunos.Tabela`(ID,Nome,Telefone,Matricula,Status)
-values (6,"Carla",34267644,4444,"Ativado")
+values (5,"Carla",34267644,4444,"Ativado")
