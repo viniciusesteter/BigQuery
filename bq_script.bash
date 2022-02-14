3f2dac12e73c2135b6fcd2f10afa7a9dbc1e4d8d
Vinicius <vinicius_marcondes_esteter@hotmail.com>
2022-02-14 17:26:16 -0300

    Alterado insert para um file e adicionado aspas

---
+++ bq_script.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcloud config set project credible-skill-341215
+bq query --nouse_legacy_sql \
+          'INSERT INTO credible-skill-341215.Alunos.Tabela
+          (ID,Nome,Telefone,Matricula,Status)
+            values (1,"Maria",34267699,8888,"Inativo"), 
+            (2,"Joaquim",34267677,7777,"Inativo"),
+            (3,"Luis",34267666,6666,"Inativo"),
+            (4,"Julia",34267655,5555,"Ativado"),
+            (5,"Carla",34267644,4444,"Ativado")'
