06b8f0a053e1ce7a41ad3361bc38ffae51ffaf61
Vinicius <vinicius_marcondes_esteter@hotmail.com>
2022-02-14 17:57:07 -0300

    teste

--- bq_script.sh
+++ bq_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-372.0.0-linux-arm.tar.gz
-./google-cloud-sdk/install.sh
-./google-cloud-sdk/bin/gcloud init
+run ./google-cloud-sdk/install.sh
+run ./google-cloud-sdk/bin/gcloud init
 bq query --use_legacy_sql=false --destination_table=Alunos.Tabela \
           'INSERT INTO credible-skill-341215.Alunos.Tabela
           (ID,Nome,Telefone,Matricula,Status)
