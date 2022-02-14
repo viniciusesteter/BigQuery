b2badbc38bdeb3c471614fb506326513edb6aa3c
Vinicius <vinicius_marcondes_esteter@hotmail.com>
2022-02-14 17:46:30 -0300

    teste

--- bq_script.sh
+++ bq_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gcloud config set project credible-skill-341215
 bq query --nouse_legacy_sql \
           'INSERT INTO credible-skill-341215.Alunos.Tabela
           (ID,Nome,Telefone,Matricula,Status)
