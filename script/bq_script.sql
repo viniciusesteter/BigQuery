1cf4c02ce904ce7c79b7141fe59a7e53c48d18f5
Vinicius <vinicius_marcondes_esteter@hotmail.com>
2022-02-18 16:12:34 -0300

    Alterando método para .sql do script

--- script/bq_script.sql
+++ script/bq_script.sql
@@ -1,6 +1,4 @@
- 
-         
-          INSERT INTO credible-skill-341215.Alunos.Tabela
+         INSERT INTO credible-skill-341215.Alunos.Tabela
           (ID,Nome,Telefone,Matricula,Status)
             values (1,"Maria",34267699,8888,"Inativo"), 
             (2,"Joaquim",34267677,7777,"Ativado"),
